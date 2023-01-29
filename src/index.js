205f75f656eafe5f5c80600ca26e7cb3f4ccfde4
Bohdan <nilsrondhuis@ukr.net>
2023-01-29 18:20:11 +0200

    fix basename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "components/App/App";
-import "modern-normalize";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./styles/main.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App/App';
+import 'modern-normalize';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './styles/main.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/mattress-shop">
     <React.StrictMode>
       <App />
     </React.StrictMode>
